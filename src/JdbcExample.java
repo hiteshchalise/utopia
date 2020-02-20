c7153c5a37b53b6d269e674dbfce67e3bdc8e5eb
Hitesh Chalise <hiteshchalise10@gmail.com>
2020-02-20 15:52:41 +0545

    resultset added

--- src/JdbcExample.java
+++ src/JdbcExample.java
@@ -4,16 +4,19 @@ public class JdbcExample {
 
     public static void main(String[] args) throws Exception{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        String query = "INSERT INTO book(id, author, title, price) VALUES(1, 'one', 'author', 122)";
-        String query2 = "INSERT INTO book(id, author, title, price) VALUES(1, 'one', 'author', 122)";
+        String query = "SELECT * FROM book;";
         Connection conn = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/mydb",
                 "root",
                 "pass");
         Statement stmt = conn.createStatement();
-        stmt.execute(query);
-        stmt.execute(query2);
-
+        ResultSet resultSet = stmt.executeQuery(query);
+        while(resultSet.next()){
+            int i = resultSet.getMetaData().getColumnCount();
+            for (int j = 0; j < i; j++) {
+                System.out.println("Column: " + j + " data: " + resultSet.getObject(j + 1) );
+            }
+        }
         stmt.close();
         conn.close();
     }
